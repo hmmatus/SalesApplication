35cd69501c2aa15a7470abd17bbf6f8e7e07e3d5
Hector Manrique Matus Henriquez <manriquematus1@gmail.com>
2018-05-10 00:22:39 -0600

    Agregadas funciones de manejo de consultas para venta,cliente y registro en WebService

--- app/src/main/java/com/example/manrique_matus/salesapplication/WebService/WebService.java
+++ app/src/main/java/com/example/manrique_matus/salesapplication/WebService/WebService.java
@@ -1,9 +1,11 @@
 package com.example.manrique_matus.salesapplication.WebService;
 
+import android.content.Context;
 import android.os.AsyncTask;
 
 import com.example.manrique_matus.salesapplication.Classes.Cliente;
 import com.example.manrique_matus.salesapplication.Classes.Producto;
+import com.example.manrique_matus.salesapplication.Classes.Vendedor;
 import com.example.manrique_matus.salesapplication.Classes.Venta;
 
 import java.util.ArrayList;
@@ -14,6 +16,11 @@ public class WebService extends AsyncTask<Void, Void, String>{
 
     //Campos extras
     boolean flag=true;
+    Context context;
+    int condicion;
+    Cliente cliente;
+    Vendedor vendedor;
+    Venta venta;
 
     //Arreglos de objetos
     ArrayList<Producto> productos=new ArrayList<>();
@@ -22,6 +29,34 @@ public class WebService extends AsyncTask<Void, Void, String>{
     //URL
     String ip="";
 
+    //Constructores
+
+    //Constructor vacio
+
+    public WebService() {
+    }
+
+    //Constructor con objeto Vendedor
+    public WebService(int condicion,Context context,Vendedor vendedor){
+        this.condicion=condicion;
+        this.context=context;
+        this.vendedor=vendedor;
+    }
+
+    //Constructor con objeto Cliente
+    public WebService(int condicion,Context context,Cliente cliente){
+        this.condicion=condicion;
+        this.context=context;
+        this.cliente=cliente;
+    }
+
+    //Constructor con venta
+    public WebService(int condicion,Context context,Venta venta){
+        this.condicion=condicion;
+        this.context=context;
+        this.venta=venta;
+    }
+
     @Override
     protected void onPreExecute(){
         super.onPreExecute();
@@ -29,6 +64,11 @@ public class WebService extends AsyncTask<Void, Void, String>{
 
     @Override
     protected String doInBackground(Void... voids) {
+        switch (condicion){
+            case 1://Ingresa un cliente
+
+        }
+
         return null;
     }
 
@@ -36,4 +76,23 @@ public class WebService extends AsyncTask<Void, Void, String>{
     protected void onPostExecute(String result){
 
     }
+
+
+    //Funcion que ingresa el cliente
+    private void IngresarCliente(String url,Cliente cliente){
+    }
+
+    //Funcion que ingresa la venta
+    private void IngresarVenta(String url,Venta venta){
+    }
+
+    //Funcion que recibe JSON de registros
+    private String recibirRegistros(String url,Vendedor vendedor){
+        StringBuilder constructor=null;
+        String result="";
+
+        return result;
+    }
+
+
 }
