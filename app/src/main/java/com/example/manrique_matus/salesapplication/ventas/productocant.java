542e9406bc2fd4d331fe54696a1fea6f8d60fc39
César Lima <cesarlopezlima1@gmail.com>
2018-05-10 10:16:17 -0600

    refactoring la mayuscula del xml

--- app/src/main/java/com/example/manrique_matus/salesapplication/ventas/productocant.java
+++ app/src/main/java/com/example/manrique_matus/salesapplication/ventas/productocant.java
@@ -65,7 +65,7 @@ public class productocant extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_Card_producto, container, false);
+        return inflater.inflate(R.layout.fragment_card_producto, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
